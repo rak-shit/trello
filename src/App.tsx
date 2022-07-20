6796de185df1858f2306ebb1d30e748cd037d4ea
Sai Rakshit <rakshit@esper.io>
2022-07-20 22:22:34 +0530

    added open-sans font

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import Board from './Components/Board';
 function App() {
   return (
     <div className="App">
+      <div className='nav'>
+        <h2>Trello Clone</h2>
+      </div>
       <Board />
     </div>
   )
